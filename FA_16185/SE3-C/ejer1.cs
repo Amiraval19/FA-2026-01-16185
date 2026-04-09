96c54cf7aca9b16a7aca8291df9a4dc660834978
Andree <n00519198@upn.pe>
2026-04-09 15:35:56 -0500

    Subiendo ejer 1 en c#

---
+++ FA_16185/SE3-C/ejer1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class ejer1
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+
+            Console.WriteLine("\nDatos de entrada");
+            Console.WriteLine("-----------------------");
+            Console.Write("\nIngrese su nombre :");
+            nombre = Console.ReadLine();
+            Console.Write("\nIngrese su carrera :");
+            carrera = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("\nDatos de salida");
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("\n----- IMPRESO CON + -----");
+            Console.WriteLine(nombre + ", Bienvenido al curso de Fundamentos de Algoritmos de la carrera " + carrera);
+           
+            Console.WriteLine("\n----- IMPRESO CON $ -----");
+            Console.WriteLine($"{nombre} , Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}");
+
+            Console.WriteLine("\n----- IMPRESO CON {} -----");
+            Console.WriteLine("{0}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {1}" , nombre,carrera);
+            Console.ReadKey();
+        }
+    }
+}
