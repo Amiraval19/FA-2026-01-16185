8e24c776db5b22cd78cef3c030edbc97a51a996c
x-Joshe-x <jcrisantoipq@gmail.com>
2026-04-16 15:53:03 -0500

    Actualizando ejercicio 3 en C# y py

--- FA_16185/SE3-C/ejer3.cs
+++ FA_16185/SE3-C/ejer3.cs
@@ -10,6 +10,36 @@ namespace SE3_C
     {
         static void Main(string[] args)
         {
+            /*int num1, num2;
+            double resta, suma, division, multi;
+
+            Console.Write("Ingrese numero 1: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese numero 1: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            suma = num1 + num2;
+            resta = num1 - num2;
+            division = num1 / num2;
+            multi = num1 * num2;
+
+            Console.WriteLine();
+            Console.WriteLine("Resultados :");
+            Console.WriteLine("La suma entre {0} y {1} es : {2}",num1, num2,suma);
+            Console.WriteLine("La resta entre {0} y {1} es : {2}",num1, num2,resta);
+            Console.WriteLine("La multiplicacion entre {0} y {1} es : {2}",num1, num2,multi);
+            Console.WriteLine("La division entre {0} y {1} es : {2}",num1, num2,division); */
+
+            Console.Write("Ingrese numero 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese numero 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSuma: " + (num1 + num2));
+            Console.WriteLine("\nResta: " + (num1 - num2));
+            Console.WriteLine("\nMultiplicacion: " + (num1 * num2));
+            Console.WriteLine("\nDivision: " + (double)(num1 / num2 ));
+            Console.ReadKey();
 
         }
     }
---
+++ FA_16185/SE3-P/ejer3.py
@@ -0,0 +1,17 @@
+num1 = int(input("Ingrese numero 1: "))
+num2 = int(input("Ingrese numero 2: "))
+
+#suma = num1 + num2
+#resta = num1 - num2
+#multi = num1 * num2
+#division = num1 / num2
+
+#print(f"Suma entre {num1} y {num2} es : {suma}")
+#print(f"Resta entre {num1} y {num2} es : {resta}")
+#print(f"Multiplicacion entre {num1} y {num2} es : {multi}")
+#print(f"Division entre {num1} y {num2} es : {division:.2f}")
+
+print("Suma: ", (num1 + num2))
+print("Resta: ", (num1 - num2))
+print("Multiplicacion: ", (num1 * num2))
+print("Division: ", (num1 / num2))
