2898763993fbce88ef029fd1943b2c0ce5db1586
x-Joshe-x <jcrisantoipq@gmail.com>
2026-04-16 15:06:58 -0500

    Subiendo ejercicio 1 en py

---
+++ FA_16185/SE3-P/ejer1.py
@@ -0,0 +1,13 @@
+nombre = input("Ingrese nombre :")
+carrera = input ("Ingrese la carrera :")
+
+print("\n----- IMPRESO CON + -----")
+print(nombre+", Bienvenido al curso de Fundamentos de Algoritmos de la carrera " + carrera)
+           
+print("\n----- IMPRESO CON $ -----")
+print(f"{nombre} , Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}")
+
+
+
+
+
